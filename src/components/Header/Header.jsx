bf700bb3e2b6ad2cf4eda8b6c6df18d305e951c9
OlliesWorld <rtlockwoodwork@gmail.com>
2023-03-14 11:33:15 -0600

    fix open-tech path

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -398,7 +398,7 @@ const Header = (props) => {
                     </a>
                   </li>
                   <li>
-                    <a href="/open-technologies/specifications/collections/" className="dropdown-item mb-3">
+                    <a href="/open-technologies/" className="dropdown-item mb-3">
                       <div className="row">
                         <div className="col-1 lc-icon">
                           <img className="d-block mx-auto" src="https://voyager.postman.com/icon/lifecycle.svg" height="40px" />
