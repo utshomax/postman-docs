1cc84c5cb799294d5164c264ff11ee0ce1153cd0
Christina Hastenrath <christina.hastenrath@postman.com>
2023-10-11 14:21:40 -0700

    update topnavbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { trackCustomEvent } from 'gatsby-plugin-google-analytics';
+import Dropdown from './Dropdown';
 import $ from 'jquery';
-import {
-  PrimaryNavbarV6, 
-  SecondaryNavbarV6, 
-  NavStyles, 
-  DropdownStyles, 
-  DropdownStylesSecond,
-  CTAButton
-} from './HeaderStyles.jsx' ;
-
+import {PrimaryNavbarV6, SecondaryNavbarV6, NavStyles, DropdownStyles, DropdownStylesSecond, CTAButton} from './HeaderStyles.jsx' ;
+import { SearchWrapperStyling } from '../Search/searchStyles.jsx';
+import { Paragraph } from 'aether-marketing';
+// prod nav data
 import navbarData from '../../../bff-data/navbar.json';
 import navtopicsdropdownData from '../../../bff-data/navtopicsdropdown.json';
-// For local TOPNAVBAR TESTING
+// fallback nav data
 import navbarDataLocal from '../../../build/navbarDev.json';
 import navtopicsdropdownDataLocal from '../../../build/navtopicsdropdownDev.json';
-
-import { Paragraph } from 'aether-marketing';
 
 // Get Cookie for Sign In toggler
 const getCookie = (a) => {
@@ -34,7 +28,7 @@ const LoginCheck = (props) => {
   if (!hidden) {
     return (
       <CTAButton>
-         <a
+        <a
           href={`https://go.postman${beta}.co/build`}
           className='button__sign-in pingdom-transactional-check__sign-in-button'
           style={{ padding: '4px 12px 4px 12px' }}
@@ -97,11 +91,12 @@ const LoginCheck = (props) => {
 };
 
 const Header = (props) => {
+
   const [beta, setBeta] = useState('');
   const [cookie, setCookie] = useState('');
   const [hidden, setHidden] = useState(true);
-  const [data, setData] = useState(navbarData);
-  const [dataDropdown, setDataDropdown] = useState(navbarDataLocal);
+  const [data, setData] = useState(navbarDataLocal);
+  const [dataDropdown, setDataDropdown] = useState(navtopicsdropdownData);
   const [visibleHelloBar] = useState();
 
   useEffect(() => {
@@ -110,22 +105,21 @@ const Header = (props) => {
 
     setCookie(cookie);
     setBeta(beta);
-
     const navbarKeys = ['items', 'media', 'type'];
-    const navtopicsdropdownKeys = ['items', 'type']
-
+    const navtopicsdropdownKeys = ['items', 'type'];
+    
     if (navbarKeys.every(key => Object.keys(navbarDataLocal).includes(key))) {
       setData(navbarDataLocal)
     } else {
       setData(navbarDataLocal)
     }
 
+
     if (navtopicsdropdownKeys.every(key => Object.keys(navtopicsdropdownData).includes(key))) {
       setDataDropdown(navtopicsdropdownData)
     } else {
       setDataDropdown(navtopicsdropdownDataLocal)
     }
-
 
     const { waitBeforeShow } = props;
 
@@ -384,12 +378,12 @@ const Header = (props) => {
             <DropdownStylesSecond className="dropdown position-static">
               <a
                 className="nav-link navbar-brand"
-                href="##"
+                href="/"
                 id="navbarDropdownMenuLink"
                 data-toggle="dropdown"
                 aria-expanded="false"
               >
-                Open Technologies
+                Learning Center
                 <svg
                   className="arrow-icon"
                   xmlns="http://www.w3.org/2000/svg"
@@ -414,7 +408,7 @@ const Header = (props) => {
                         </div>
                         <div className="col-11">
                           <Paragraph className="strong mb-0" >{item.title} <svg className="arrow-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="#212121"><g><path d="M10.375,3.219,6,6.719l-4.375-3.5A1,1,0,1,0,.375,4.781l5,4a1,1,0,0,0,1.25,0l5-4a1,1,0,0,0-1.25-1.562Z"></path></g></svg></Paragraph>
-                          <Paragraph className="dropdown-item-text-wrap small" style={{    'color': '#707070 !important'}}>{item.body}</Paragraph>
+                          <Paragraph className="dropdown-item-text-wrap small" >{item.body}</Paragraph>
                         </div>
                       </div>
                     </a>
@@ -424,8 +418,6 @@ const Header = (props) => {
                 </ul>
               </div>
             </DropdownStylesSecond>
-
-          {/* </DropdownStylesSecond> */}
           <button
             onClick={() => {
               showTargetElementLC();
@@ -456,17 +448,25 @@ const Header = (props) => {
             className="collapse navbar-collapse"
             id="navbarSupportedContentBottom"
           >
-            <ul className="property-context-menu navbar-nav ml-auto">
-            <li className="nav-item">
-                <a
-                  className="nav-link uber-nav"
-                  href="https://www.postman.com/open-technologies/"
-                >
-                  Who we are
-                </a>
-              </li>
-            </ul>
-           
+            {/* Algolia Widgets */}
+            <SearchWrapperStyling className="form-inline header__search  ml-auto">
+              <svg
+                className="nav-search__icon"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="#6b6b6b"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M9.87147 9.16437C10.5768 8.30243 11 7.20063 11 6C11 3.23858 8.76142 1 6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11C7.20063 11 8.30243 10.5768 9.16437 9.87147L9.89648 10.6036L9.64648 10.8536L13.5758 14.7829C13.8101 15.0172 14.19 15.0172 14.4243 14.7829L14.7829 14.4243C15.0172 14.19 15.0172 13.8101 14.7829 13.5758L10.8536 9.64648L10.6036 9.89648L9.87147 9.16437ZM6 10C8.20914 10 10 8.20914 10 6C10 3.79086 8.20914 2 6 2C3.79086 2 2 3.79086 2 6C2 8.20914 3.79086 10 6 10Z"
+                />
+              </svg>
+
+              <Dropdown />
+            </SearchWrapperStyling>
           </div>
         </NavStyles>
       </SecondaryNavbarV6>
